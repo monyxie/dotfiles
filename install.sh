150d9bcd8724fc698cfb34232359cca76993713b
Xie Wanneng <xwn.sk@qq.com>
2016-01-20 11:46:07 +0800

    add .gitconfig to install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ pushd `dirname $0` > /dev/null
 DIR=`pwd`
 popd > /dev/null
 
-echo "Script path: $DIR"
-
 if [[ $OS == 'Linux' ]]; then
+  ln -s $DIR/.gitconfig ~/.gitconfig
   ln -s $DIR/.vimrc ~/.vimrc
   ln -s $DIR/vimfiles ~/.vim
   ln -s $DIR/.tmux.conf ~/.tmux.conf
   ln -s $DIR/rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
 elif [[ $OS == 'Darwin' ]]; then
+  ln -s $DIR/.gitconfig ~/.gitconfig
   ln -s $DIR/.vimrc ~/.vimrc
   ln -s $DIR/vimfiles ~/.vim
   ln -s $DIR/.tmux.conf ~/.tmux.conf
