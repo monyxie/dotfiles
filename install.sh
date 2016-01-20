ab958373881d0ed4acaebaf99bacf656685591e6
xwn <xwn.sk@qq.com>
2016-01-20 11:40:49 +0800

    osx support

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-ln -s -r ./.vimrc ~/.vimrc
-ln -s -r ./.tmux.conf ~/.tmux.conf
-ln -s -r ./rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
+OS=$(uname)
+pushd `dirname $0` > /dev/null
+DIR=`pwd`
+popd > /dev/null
+
+echo "Script path: $DIR"
+
+if [[ $OS == 'Linux' ]]; then
+  ln -s $DIR/.vimrc ~/.vimrc
+  ln -s $DIR/vimfiles ~/.vim
+  ln -s $DIR/.tmux.conf ~/.tmux.conf
+  ln -s $DIR/rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
+elif [[ $OS == 'Darwin' ]]; then
+  ln -s $DIR/.vimrc ~/.vimrc
+  ln -s $DIR/vimfiles ~/.vim
+  ln -s $DIR/.tmux.conf ~/.tmux.conf
+  ln -s $DIR/rime/default.custom.yaml ~/Library/Rime/default.custom.yaml
+else
+  echo 'Unsupported OS.'
+fi
