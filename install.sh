62d8e7f9367db2225e0742a29d74c53417ea598b
Xie Wanneng <xwn.sk@qq.com>
2016-01-20 11:52:18 +0800

    improve install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,15 @@ pushd `dirname $0` > /dev/null
 DIR=`pwd`
 popd > /dev/null
 
+ln -s $DIR/.gitconfig ~/.gitconfig
+ln -s $DIR/.vimrc ~/.vimrc
+ln -s $DIR/vimfiles ~/.vim
+ln -s $DIR/.tmux.conf ~/.tmux.conf
+
 if [[ $OS == 'Linux' ]]; then
-  ln -s $DIR/.gitconfig ~/.gitconfig
-  ln -s $DIR/.vimrc ~/.vimrc
-  ln -s $DIR/vimfiles ~/.vim
-  ln -s $DIR/.tmux.conf ~/.tmux.conf
   ln -s $DIR/rime/default.custom.yaml ~/.config/ibus/rime/default.custom.yaml
 elif [[ $OS == 'Darwin' ]]; then
-  ln -s $DIR/.gitconfig ~/.gitconfig
-  ln -s $DIR/.vimrc ~/.vimrc
-  ln -s $DIR/vimfiles ~/.vim
-  ln -s $DIR/.tmux.conf ~/.tmux.conf
   ln -s $DIR/rime/default.custom.yaml ~/Library/Rime/default.custom.yaml
 else
-  echo 'Unsupported OS.'
+  echo 'Unsupported OS. Rime config is not installed.'
 fi
