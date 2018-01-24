6df90104f88a8544e87970088b8fafdbab4b99fd
Mony Xie <monyxie@gmail.com>
2018-01-24 15:25:37 +0800

    install script revamp

--- install.sh
+++ install.sh
@@ -1,43 +1,93 @@
 #!/bin/bash
 
 OS=$(uname)
-DIR=$(dirname $(readlink -f $0))
+DIR=$(dirname $(readlink -f --canonicalize $0))
 
-if [[ "$OS" != 'Linux' ]]; then
-  echo 'Unsupported OS.'
+if [[ "$OS" != 'Linux' ]]
+then
+  echo "[ERROR] Dude, this ain't Linux."
   exit -1
 fi
 
-
-### HOME
-ln -s "$DIR/.gitconfig" "$HOME/.gitconfig"
-ln -s "$DIR/.gitignore_global" "$HOME/.gitignore_global"
-ln -s "$DIR/.inputrc" "$HOME/.inputrc"
-ln -s "$DIR/.tmux.conf" "$HOME/.tmux.conf"
-ln -s "$DIR/vimfiles" "$HOME/.vim"
-
-
-### HOME/.CONFIG
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s "$DIR/vimfiles" "$XDG_CONFIG_HOME/nvim"
-ln -s "$DIR/vimfiles/vimrc" "$XDG_CONFIG_HOME/nvim/init.vim"
-ln -s "$DIR/.config/caja/scripts" "$XDG_CONFIG_HOME/caja/scripts"
-ln -s "$DIR/.config/ibus/rime/default.custom.yaml" "$XDG_CONFIG_HOME/ibus/rime/default.custom.yaml"
-ln -s "$DIR/.config/redshift.conf" "$XDG_CONFIG_HOME/redshift.conf"
+
+SYMLINKS=$(cat <<HEREDOC
+$DIR/.gitconfig <- $HOME/.gitconfig
+$DIR/.gitignore_global <- $HOME/.gitignore_global 
+$DIR/.inputrc <- $HOME/.inputrc
+$DIR/.tmux.conf <- $HOME/.tmux.conf
+$DIR/vimfiles <- $HOME/.vim
+# $DIR/vimfiles <- $XDG_CONFIG_HOME/nvim
+# $DIR/vimfiles/vimrc <- $XDG_CONFIG_HOME/nvim/init.vim
+$DIR/.config/caja/scripts <- $XDG_CONFIG_HOME/caja/scripts
+$DIR/.config/ibus/rime/default.custom.yaml <- $XDG_CONFIG_HOME/ibus/rime/default.custom.yaml
+$DIR/.config/redshift.conf <- $XDG_CONFIG_HOME/redshift.conf
+HEREDOC
+)
+
+echo "$SYMLINKS" | sed 's/ \+<- \+/\t/g' | while IFS=$'\t' read -r COL1 COL2
+do
+    if [ "${COL1:0:1}" == "#" ]
+    then
+        continue
+    fi
+
+    if  [ "$COL1" == "" ] || [ "$COL2" == "" ]
+    then
+        echo "[ERROR] invalid entry: $COL1 <- $COL2"
+        continue
+    fi
+
+    if [ ! -e "$COL1" ]
+    then
+        echo "[ERROR] target file does not exist: $COL1"
+        continue
+    fi
+
+    if [ -e "$COL2" ]
+    then
+        if [ "$COL1" == $(readlink -n --canonicalize "$COL2") ]
+        then
+            echo "[OK] already symlinked: $COL1"
+        else
+            echo "[ERROR] file exists: $COL2"
+        fi
+        continue
+    fi
+
+    ERR=$(ln -sT "$COL1" "$COL2" 2>&1)
+    if [ "$?" == '0' ]
+    then
+        echo "[OK] $COL1"
+    else
+        echo "[ERROR] $ERR"
+    fi
+done
 
 
 ### BASHRC
-# source ./.bashrc in ~/.bashrc
-if [ -f "$HOME/.bashrc" ] ; then
-	grep -F '{{DOTFILESTAGABCD1234}}' "$HOME/.bashrc" > /dev/null; result=${?}
-	if [ ${result} -eq 0 ] ; then
-		echo '.bashrc is already installed. Skipping.'
-	else
-		echo '# {{DOTFILESTAGABCD1234}}' >> "$HOME/.bashrc"
-		echo 'if [ -f "'${DIR}'/.bashrc" ]; then' >> "$HOME/.bashrc"
-		echo '	source "'${DIR}'/.bashrc"' >> "$HOME/.bashrc"
-		echo 'fi' >> "$HOME/.bashrc"
-	fi
+if [ ! -f "$HOME/.bashrc" ]
+then
+    touch "$HOME/.bashrc"
+fi
+
+MARK='{{DOTFILESTAGABCD1234}}'
+SOURCE=$(cat <<HEREDOC
+
+### $MARK ###
+if [ -f '${DIR}/.bashrc' ]
+then
+    source '${DIR}/.bashrc'
+fi
+### $MARK ###
+HEREDOC
+)
+
+grep -F "$MARK" "$HOME/.bashrc" > /dev/null
+if [ $? -eq 0 ]
+then
+    echo '[OK] .bashrc is already sourced.'
 else
-	echo '~/.bashrc does not exist. Skip sourcing.'
+    echo "$SOURCE" >> "$HOME/.bashrc"
+    echo '[OK] .bashrc is installed.'
 fi
